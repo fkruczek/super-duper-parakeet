6a77e754dd666c239509dd1f6f30b2c75a76013c
fkruczek <fryderykkruczek@gmail.com>
2017-11-18 16:36:32 +0100

    zadanie 2

---
+++ zadania1/zad2/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main(){
+    int a,b,c;
+    char znak;
+    do{
+        printf("Podaj 3 liczby: ");
+        scanf("%d %d %d",&a,&b,&c);
+        if(a>0 && b>0 && c>0){
+            if(a>=b && a>=c){
+                if((b+c)>a) printf("To moze byc trojkat");
+                else printf("To nie moze byc trojkat");
+            }
+            else if(b>=a && b>=c){
+                if((a+c)>b) printf("To moze byc trojkat");
+                else printf("To nie moze byc trojkat");
+            }
+            else if(c>=b && c>=a){
+                if((b+a)>c) printf("To moze byc trojkat");
+                else printf("To nie moze byc trojkat");
+            }
+        }
+        else printf("To nie moze byc trojkat");
+        printf("\nJeszcze raz? [T]\n");
+        znak = getch();
+    }while(znak == 'T' || znak == 't');
+}
