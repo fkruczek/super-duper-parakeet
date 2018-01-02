ce8dbd395ffef9352a9b76b2404144d6ea0447c0
fkruczek <fryderykkruczek@gmail.com>
2018-01-02 23:50:09 +0100

    binary files

--- zadania3/bin/main.c
+++ zadania3/bin/main.c
@@ -15,22 +15,24 @@ struct student srednia(struct student s);
 double maksimum(void); // funkcja zwraza najwieksza srednia
 struct student *najlepsi(int *n); // zwraca wskaznik do tablicy struktur (echhhhhhhhhhhh)
 double minimum(void); // funkcja zwraca minimalna srednia
-int usun (void); //usuwanie studentów z najniższ srednia
+int usun(void); //usuwanie studentów z najniższ srednia
 void popraw (void); // znaleźć studenta o podanych danych i poprawia jego dane
 
 int main()
 {
     int i, il_najlepszych=0;
     struct student *najlepsi_studenci=NULL;
-//    dodaj();
-//    dodaj();
-//    dodaj();
+    dodaj();
+    dodaj();
+    dodaj();
     drukuj_plik();
     printf("\nNazwiska osob, ktore maja maksymalna srednia wynoszaca %lf to: \n", maksimum());
     najlepsi_studenci=najlepsi(&il_najlepszych);
     for (i=0; i<il_najlepszych; i++){
         printf("%s\n",(*(najlepsi_studenci+i)).nazwisko);
     }
+    printf("Usunieto %d studentow\n",usun());
+    drukuj_plik();
     return 0;
 }
 void dodaj(void)
@@ -104,6 +106,7 @@ double maksimum(void)
     {
         if(s.srednia>maks_srednia) maks_srednia=s.srednia;
     }
+    fclose(studenci);
     return maks_srednia;
 }
 struct student *najlepsi(int *n)
@@ -122,5 +125,44 @@ struct student *najlepsi(int *n)
             *(najlepsi_studenci+(*n)-1)=s;
         }
     }
+    fclose(studenci);
     return najlepsi_studenci;
+}
+double minimum(void)
+{
+    FILE *studenci;
+    struct student s;
+    double minimalna=10;
+    studenci=fopen(FILENAME,"rb");
+
+    while(fread(&s, sizeof(struct student), 1, studenci)==1)
+    {
+        if(s.srednia<minimalna) minimalna=s.srednia;
+    }
+    fclose(studenci);
+    return minimalna;
+}
+
+int usun(void)
+{
+    FILE *studenci, *nowy;
+    double minimalna=minimum();
+    struct student s;
+    int usunietych=0;
+    studenci=fopen(FILENAME, "rb");
+    nowy=fopen("nowy.bin", "wb");
+    while(fread(&s, sizeof(struct student),1, studenci)){
+        if(s.srednia==minimalna){
+            printf("Usunieto studenta o imieniu %s\n",s.nazwisko);
+            usunietych++;
+          }
+        else{
+            fwrite(&s, sizeof(struct student), 1, nowy);
+        }
+    }
+    fclose(studenci);
+    fclose(nowy);
+    remove(FILENAME);
+    rename("nowy.bin", FILENAME);
+    return usunietych;
 }
