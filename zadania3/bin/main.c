068c8dea5267be96d41bf498820464282f599c5e
fkruczek <fryderykkruczek@gmail.com>
2018-01-02 21:41:17 +0100

    binary files

--- zadania3/bin/main.c
+++ zadania3/bin/main.c
@@ -12,13 +12,25 @@ void dodaj(void);
 void drukuj_plik(void);
 void drukuj(struct student);
 struct student srednia(struct student s);
+double maksimum(void); // funkcja zwraza najwieksza srednia
+struct student *najlepsi(int *n); // zwraca wskaznik do tablicy struktur (echhhhhhhhhhhh)
+double minimum(void); // funkcja zwraca minimalna srednia
+int usun (void); //usuwanie studentów z najniższ srednia
+void popraw (void); // znaleźć studenta o podanych danych i poprawia jego dane
 
 int main()
 {
-    dodaj();
-    dodaj();
-    dodaj();
+    int i, il_najlepszych=0;
+    struct student *najlepsi_studenci=NULL;
+//    dodaj();
+//    dodaj();
+//    dodaj();
     drukuj_plik();
+    printf("\nNazwiska osob, ktore maja maksymalna srednia wynoszaca %lf to: \n", maksimum());
+    najlepsi_studenci=najlepsi(&il_najlepszych);
+    for (i=0; i<il_najlepszych; i++){
+        printf("%s\n",(*(najlepsi_studenci+i)).nazwisko);
+    }
     return 0;
 }
 void dodaj(void)
@@ -81,4 +93,34 @@ struct student srednia(struct student s)
         suma+=s.oceny[i];
     s.srednia=suma/5;
     return s;
+}
+double maksimum(void)
+{
+    FILE *studenci;
+    struct student s;
+    double maks_srednia=0;
+    studenci=fopen(FILENAME,"rb");
+    while(fread(&s, sizeof(struct student), 1, studenci)==1)
+    {
+        if(s.srednia>maks_srednia) maks_srednia=s.srednia;
+    }
+    return maks_srednia;
+}
+struct student *najlepsi(int *n)
+{
+    FILE *studenci;
+    struct student s;
+    struct student *najlepsi_studenci=NULL;
+    double max=maksimum();
+    studenci=fopen(FILENAME,"rb");
+    while(fread(&s, sizeof(struct student), 1, studenci)==1)
+    {
+        if(s.srednia==max)
+        {
+            (*n)++;
+            najlepsi_studenci=(struct student*)realloc(najlepsi_studenci, (*n)*sizeof(struct student));
+            *(najlepsi_studenci+(*n)-1)=s;
+        }
+    }
+    return najlepsi_studenci;
 }
